833dcf82434a6735815490370f2da5c3697344ed
jinnnnyh <dudgus0799@gmail.com>
2024-07-29 21:44:55 +0900

    예약 날짜 데이터로 시간 비활성화

--- src/main/java/bitc/fullstack405/team2/cafeResrvation/LocaResController.java
+++ src/main/java/bitc/fullstack405/team2/cafeResrvation/LocaResController.java
@@ -35,17 +35,12 @@ public class LocaResController {
 
     return mv;
   }
-//
-//  @GetMapping("/test_reservation")
-//  public String selectDate(){
-//    return "/reservation/test_writeReservation";
-//  }
 
-//  @ResponseBody
-//  @GetMapping("/hongdae/1/{date}")
-//  public String selectDate(String date){
-//   return date;
-//  }
+
+  @GetMapping("/{cafeName}/{cafeIdx}/{date}")
+  public String selectDate(@PathVariable(name = "date") String date){
+   return date;
+  }
 
   // 예약 정보 입력 페이지로 이동
   @RequestMapping("/{cafeName}/{cafeIdx}/{themeIdx}/{time}")
